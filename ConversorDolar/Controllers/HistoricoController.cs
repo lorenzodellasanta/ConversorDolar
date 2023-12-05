fabec330a93381579249119c41bf45df6e49631a
Lorenzo Moura Della Santa <lorenzo.moura@unidesk.com.br>
2023-12-05 15:16:59 -0300

    comecando o controller e a view de historico

--- ConversorDolar/Controllers/HistoricoController.cs
+++ ConversorDolar/Controllers/HistoricoController.cs
@@ -1,6 +1,20 @@
-﻿namespace ConversorDolar.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorDolar.Controllers
 {
-    public class HistoricoController
+    public class HistoricoController : Controller
     {
+        private readonly ILogger<HistoricoController> _logger;
+        private readonly HttpClient _httpClient = new();
+
+        public HistoricoController(ILogger<HistoricoController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
     }
 }
